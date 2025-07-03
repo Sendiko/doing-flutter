dfda455abebd44c781991f17181fc9920f1a840b
Sendiko <sarangtawon897@gmail.com>
2025-07-03 10:25:51 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# doing_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/doing_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doing_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
