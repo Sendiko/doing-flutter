3e5be40bc62520a1672def9ad1e64e7e752b1c12
Sendiko <sarangtawon897@gmail.com>
2025-07-16 10:23:31 +0700

    chore: clean up main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
-import 'package:doing_flutter/add_item_screen.dart';
-import 'package:doing_flutter/todo_model.dart';
+import 'package:doing_flutter/todo/home_screen.dart';
+import 'package:doing_flutter/todo/todo_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,166 +18,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<Todo> _todos = [];
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchTodos();
-  }
-
-  Future<void> _fetchTodos() async {
-    final uri = Uri.parse('https://doingflutter.sendiko.my.id/todos');
-    print('Fetching data from: $uri');
-    try {
-      final response = await http.get(uri);
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        setState(() {
-          _todos = data.map((item) => Todo.fromJson(item)).toList();
-          _isLoading = false;
-        });
-        print('Data fetched and parsed successfully!');
-      } else {
-        setState(() {
-          _isLoading = false;
-        });
-        print('Server error: ${response.statusCode}');
-      }
-    } catch (e) {
-      setState(() {
-        _isLoading = false;
-      });
-      print('An error occurred: $e');
-    }
-  }
-
-  Future<void> _updateTodoStatus(Todo todo) async {
-    final uri = Uri.parse(
-      'https://doingflutter.sendiko.my.id/todos/${todo.id}',
-    );
-    try {
-      final response = await http.put(
-        uri,
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode({'completed': !todo.completed, 'title': todo.title}),
-      );
-
-      if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Item updated successfully')),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Failed to update item, see logs for more detail'),
-          ),
-        );
-        print('Server error: ${response.statusCode}');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Failed to update item: $e')));
-    }
-  }
-
-  Future<void> _deleteTodo(int id) async {
-    final uri = Uri.parse("https://doingflutter.sendiko.my.id/todos/$id");
-
-    try {
-      final response = await http.delete(uri);
-
-      if (response.statusCode == 204) {
-        setState(() {
-          _todos.removeWhere((todo) => todo.id == id);
-        });
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Item deleted successfully')),
-        );
-      } else {
-        print("Error deleting: ${response.statusCode}");
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Failed to delete item, see logs for more detail'),
-          ),
-        );
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Failed to delete item: $e')));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('API To Do List'),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final result = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddItemScreen()),
-          );
-          if (result == true) {
-            setState(() {
-              _isLoading = true;
-            });
-            _fetchTodos();
-          }
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _todos.length,
-              itemBuilder: (context, index) {
-                final todo = _todos[index];
-                return ListTile(
-                  title: Text(todo.title),
-                  leading: CircleAvatar(child: Text(todo.id.toString())),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        todo.completed ? Icons.check_circle : Icons.circle,
-                        color: todo.completed ? Colors.green : Colors.grey,
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          _deleteTodo(todo.id);
-                        },
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                      ),
-                    ],
-                  ),
-                  onTap: () {
-                    _updateTodoStatus(todo);
-                    _fetchTodos();
-                  },
-                );
-              },
-            ),
+      home: BlocProvider(
+        create: (context) => TodoCubit(),
+        child: const HomeScreen(), 
+        ),
     );
   }
 }
